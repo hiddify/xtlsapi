f877f37c865c63a566b82f15b68497dedcaa8d0a
hiddify <hiddify@users.noreply.github.com>
2023-01-13 18:12:06 +0000

    ✅ Ready to clone and code.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from xtlsapi.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "xtlsapi"
--- xtlsapi/__main__.py
+++ xtlsapi/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for xtlsapi."""
 
 from .cli import main  # pragma: no cover
 
