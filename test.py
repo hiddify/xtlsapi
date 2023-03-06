30d3888164e42a5e9818ccb3505e82487493885a
hiddify <hiddify@gmail.com>
2023-03-06 23:38:33 +0100

    new: add vless,trojan,shadowsocks account creatation

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from xtlsapi import XrayClient, utils, exceptions
 
 xray_client = XrayClient('127.0.0.1', 10085)
 
-user_email = 'uuid@hiddify.com'
+uuid='9623a083-b4bf-4dac-83c2-02ba9b948714'
+user_email = f'{uuid}@hiddify.com'
+protocol='vmess'# vless,trojan,vmess,shadowsocks
 inbound_tag = 'vmess-ws'
 
 # Get stats
@@ -10,6 +12,26 @@ print(utils.human_readable_bytes(xray_client.get_client_download_traffic(user_em
 print(utils.human_readable_bytes(xray_client.get_client_upload_traffic(user_email)))
 print(utils.human_readable_bytes(xray_client.get_inbound_download_traffic(inbound_tag)))
 print(utils.human_readable_bytes(xray_client.get_inbound_upload_traffic(inbound_tag)))
+
+
+user=None
+# Add & Remove client
+if protocol=='vmess':
+    #vmess
+    user = xray_client.add_client(inbound_tag, uuid, user_email,'vmess',alter_id=0)
+elif protocol=='vless':
+    #vless
+    user = xray_client.add_client(inbound_tag, uuid, user_email,'vless',flow="xtls-rprx-vision")
+elif protocol=='trojan':
+    #trojan
+    user = xray_client.add_client(inbound_tag, uuid, user_email,'trojan')
+elif protocol=='shadowsocks':
+    #shadowsocks
+    user = xray_client.add_client(inbound_tag, uuid, user_email,'shadowsocks',cipher='chacha20_poly1305')
+
+if user:
+    print(user)
+    xray_client.remove_client(inbound_tag, user_email)
 
 # restart logger
 # xray_client.restart_logger()
--- xtlsapi/api_services/handler/add_client.py
+++ xtlsapi/api_services/handler/add_client.py
@@ -1,5 +1,5 @@
 from grpc._channel import _InactiveRpcError
-from xtlsapi.xray_api.proxy.vmess import account_pb2
+
 from xtlsapi.xray_api.common.protocol import user_pb2
 from xtlsapi.xray_api.app.proxyman.command import command_pb2
 from xtlsapi.ext import utils
@@ -8,8 +8,39 @@ from xtlsapi import exceptions
 
 
 class AddClient(BaseService):
-    def add_client(self, inbound_tag, user_id, email, level=None, alter_id=None):
+    def add_client(self, inbound_tag, user_id_or_password, email,protocol, level=None, alter_id=None,flow=None,cipher=None):
         try:
+            if protocol=='vmess':
+                from xtlsapi.xray_api.proxy.vmess import account_pb2
+                account=account_pb2.Account(id=user_id_or_password, alter_id=alter_id)
+            elif protocol=='vless':
+                from xtlsapi.xray_api.proxy.vless import account_pb2
+                account=account_pb2.Account(id=user_id_or_password,flow=flow,encryption="none")
+            elif protocol=='trojan':
+                from xtlsapi.xray_api.proxy.trojan import config_pb2
+                account=config_pb2.Account(password=user_id_or_password,flow=flow)
+            elif protocol=='shadowsocks':
+                from xtlsapi.xray_api.proxy.shadowsocks import config_pb2
+                cipher_map={'aes_128_gcm':config_pb2.CipherType.AES_128_GCM,
+                            'aes_256_gcm':config_pb2.CipherType.AES_256_GCM,
+                            'chacha20_poly1305':config_pb2.CipherType.CHACHA20_POLY1305,
+                            'xchacha20_poly1305':config_pb2.CipherType.XCHACHA20_POLY1305,
+                            None:config_pb2.CipherType.NONE                            
+                            }
+                account=config_pb2.Account(password=user_id_or_password,cipher_type=cipher_map.get(cipher,cipher_map[None]))
+            # elif protocol=='shadowsocks_2022':
+                # from xtlsapi.xray_api.proxy.shadowsocks import config_pb2
+                # cipher_map={'aes_128_gcm':config_pb2.CipherType.AES_128_GCM,
+                #             'aes_256_gcm':config_pb2.CipherType.AES_256_GCM,
+                #             'chacha20_poly1305':config_pb2.CipherType.CHACHA20_POLY1305,
+                #             'xchacha20_poly1305':config_pb2.CipherType.XCHACHA20_POLY1305,
+                #             None:config_pb2.CipherType.NONE                            
+                #             }
+                # account=config_pb2.Account(password=user_id_or_password,cipher_type=cipher_map.get(cipher,cipher_map[None])
+                # raise Exception("not implemented ")
+            else:
+                raise Exception("not implemented "+protocol )
+            
             self.handler_stub.AlterInbound(
                 command_pb2.AlterInboundRequest(
                     tag=inbound_tag,
@@ -18,9 +49,7 @@ class AddClient(BaseService):
                             user=user_pb2.User(
                                 email=email,
                                 level=level,
-                                account=utils.to_typed_message(
-                                    account_pb2.Account(id=user_id, alter_id=alter_id)
-                                ),
+                                account=utils.to_typed_message(account),
                             )
                         )
                     ),
